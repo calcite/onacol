29570c94e1873eb015cdcc167f5b9e66c8fb0f80
Josef <jnevrly@alps.cz>
2021-07-23 11:05:55 +0200

    Bump version: 0.2.0 → 0.2.1

--- onacol/__init__.py
+++ onacol/__init__.py
@@ -2,6 +2,6 @@
 
 __author__ = """Josef Nevrly"""
 __email__ = 'josef.nevrly@gmail.com'
-__version__ = '0.2.0'
+__version__ = '0.2.1'
 
 from .onacol import ConfigManager, OnacolException, ConfigValidationError
